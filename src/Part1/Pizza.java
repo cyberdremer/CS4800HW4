b22272be20faec7496e55133483fb8f99269e26b
cyberdremer <dlara7913@gmail.com>
2024-03-15 17:53:34 -0700

    -Refactored Main

--- src/Part1/Pizza.java
+++ src/Part1/Pizza.java
@@ -1,10 +1,11 @@
 package Part1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Pizza {
-    private final List<String> toppings;
+    private final ArrayList<String> toppings;
     private final PizzaSize size;
 
     private final String chainName;
@@ -37,7 +38,7 @@ public class Pizza {
         pizzaInformation.append("Chain: " + this.chainName + "\n" + "Size: " + this.size.toString() +
                 "\nToppings: ");
         for (String topping: this.toppings){
-            pizzaInformation.append(topping);
+            pizzaInformation.append(topping +" ");
         }
         return pizzaInformation.toString();
 
@@ -46,37 +47,14 @@ public class Pizza {
 
 
     public static class PizzaBuilder{
-        private List<String> pizzaToppings;
+        private HashSet<String> pizzaToppings;
         private PizzaSize size;
 
         private String chainName = "None";
 
-        public static final ArrayList<String> menuToppings = new ArrayList<String>();
-        static {
-            menuToppings.add("Extra Cheese");
-            menuToppings.add("Pepperoni");
-            menuToppings.add("Mushrooms");
-            menuToppings.add("Bacon");
-            menuToppings.add("Onions");
-            menuToppings.add("Sausage");
-
-            menuToppings.add("Peppers");
-            menuToppings.add("Chicken");
-            menuToppings.add("Olives");
-            menuToppings.add("Spinach");
-            menuToppings.add("Tomato and Basil");
-            menuToppings.add("Beef");
-
-            menuToppings.add("Ham");
-            menuToppings.add("Pesto");
-            menuToppings.add("Spicy");
-            menuToppings.add("Pork");
-
-
-
-        }
 
         public PizzaBuilder(PizzaSize size){
+            pizzaToppings = new HashSet<>();
             this.size = size;
 
         }
@@ -87,19 +65,96 @@ public class Pizza {
 
         }
 
-        public PizzaBuilder setToppings(List<String> toppings) {
-            this.pizzaToppings = new ArrayList<String>();
-            for (String topping: toppings){
-                this.pizzaToppings.add(topping + " ");
-            }
+        public PizzaBuilder setToppingPeperoni(){
+            pizzaToppings.add("Peperoni");
+            return this;
+        }
+
+        public PizzaBuilder setToppingSausage(){
+            pizzaToppings.add("Sausage");
+            return this;
+        }
+
+        public PizzaBuilder setToppingMushrooms(){
+            pizzaToppings.add("Mushrooms");
+            return this;
+        }
+
+        public PizzaBuilder setToppingBacon(){
+            pizzaToppings.add("Bacon");
+            return this;
+        }
+
+        public PizzaBuilder setToppingOnions(){
+            pizzaToppings.add("Onions");
+            return this;
+        }
+
+        public PizzaBuilder setToppingExtraCheese(){
+            pizzaToppings.add("Extra Cheese");
+            return this;
+        }
+
+        public PizzaBuilder setToppingPeppers(){
+            pizzaToppings.add("Peppers");
+            return this;
+        }
+
+        public PizzaBuilder setToppingChicken(){
+            pizzaToppings.add("Chicken");
+            return this;
+        }
+
+        public PizzaBuilder setToppingOlives(){
+            pizzaToppings.add("Olives");
             return this;
 
         }
 
+        public PizzaBuilder setToppingSpinach(){
+            pizzaToppings.add("Spinach");
+            return this;
+        }
+
+        public PizzaBuilder setToppingTomatoAndBasil(){
+            pizzaToppings.add("Tomato and Basil");
+            return this;
+        }
+
+        public PizzaBuilder setToppingBeef(){
+            pizzaToppings.add("Beef");
+            return this;
+        }
+
+        public PizzaBuilder setToppingHam(){
+            pizzaToppings.add("Ham");
+            return this;
+        }
+
+        public PizzaBuilder setToppingPesto(){
+            pizzaToppings.add("Pesto");
+            return this;
+        }
+
+        public PizzaBuilder setToppingSpicyPork(){
+            pizzaToppings.add("Spicy Pork");
+            return this;
+        }
+
+        public PizzaBuilder setToppingHamAndPineapple(){
+            pizzaToppings.add("Ham and Pineapple");
+            return this;
+        }
+
+
+
+
 
 
         public Pizza build(){
-            Pizza newPizza = new Pizza(this.size, this.pizzaToppings, this.chainName);
+            ArrayList<String> finalPizzaToppingList = new ArrayList<>();
+            finalPizzaToppingList.addAll(this.pizzaToppings);
+            Pizza newPizza = new Pizza(this.size, finalPizzaToppingList, this.chainName);
             return newPizza;
         }
     }
